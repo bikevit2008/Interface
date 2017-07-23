649870477d32d9129b0cf4f022e70c886a64f5fc
Vitaly <bikevit2008@rambler.ru>
2017-07-23 20:30:58 +0300

    Canvas sized by image size

--- src/components/InputImageComponent.js
+++ src/components/InputImageComponent.js
@@ -1,4 +1,6 @@
 import React, { PropTypes, Component } from 'react'
+import { Layer, Stage, Image } from 'react-konva';
+
 
 export default class InputImageComponent extends Component {
   onChangeFileImage(e) {
@@ -8,9 +10,31 @@ export default class InputImageComponent extends Component {
     console.log(urlObj)
     this.props.setImage(urlObj)
   }
+ state = {
+  image: null,
+  width: 0,
+  height: 0
+}
+  componentDidUpdate() {
+  const image = new window.Image();
+  image.src = this.props.file;
+  image.onload = () => {
+    this.setState({
+      image: image,
+      width: image.width,
+      height: image.height
+    });
+  }
+}
+ 
   render() {
     const { file } = this.props
-    return <div className='ib page'>     
+    return <div className='ib page'>
+              <Stage width={this.state.width} height={this.state.height}>
+                  <Layer>
+                    <Image image={this.state.image} />
+                  </Layer>
+              </Stage>
         <img src={file} />
    <p>Выберите изображение</p>
        <input type='file' onChange={::this.onChangeFileImage} />
