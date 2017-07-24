422540c064e9224a0ae5429747dd47a7a8dc7001
Vitaly <bikevit2008@rambler.ru>
2017-07-24 22:52:29 +0300

    InputImageComponent became an independent

--- src/components/InputImageComponent.js
+++ src/components/InputImageComponent.js
@@ -1,5 +1,4 @@
-import React, { PropTypes, Component } from 'react'
-import { Layer, Stage, Image } from 'react-konva';
+import React, { Component } from 'react'
 
 
 export default class InputImageComponent extends Component {
@@ -10,38 +9,13 @@ export default class InputImageComponent extends Component {
     console.log(urlObj)
     this.props.setImage(urlObj)
   }
- state = {
-  image: null,
-  width: 0,
-  height: 0
-}
-  componentDidUpdate() {
-  const image = new window.Image();
-  image.src = this.props.file;
-  image.onload = () => {
-    this.setState({
-      image: image,
-      width: image.width,
-      height: image.height
-    });
-  }
-}
  
-  render() {
-    const { file } = this.props
-    return <div className='ib page'>
-              <Stage width={this.state.width} height={this.state.height}>
-                  <Layer>
-                    <Image image={this.state.image} />
-                  </Layer>
-              </Stage>
-        <img src={file} />
-   <p>Выберите изображение</p>
-       <input type='file' onChange={::this.onChangeFileImage} />
-    </div>
-  }
-}
-
-InputImageComponent.propTypes = {
-  file: PropTypes.object.isRequired
+    render() {
+        return (
+            <div>
+                <p>Выберите изображение</p>
+                    <input type='file' onChange={::this.onChangeFileImage} />
+            </div>
+        )
+    }
 }
