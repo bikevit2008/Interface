85f8adfce5991d9ef265f6aa3c3e5a3d969f4520
Vitaly <bikevit2008@rambler.ru>
2017-07-25 16:14:23 +0300

    Changes to use canvas

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,12 @@
 
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-import InputImageComponent from './InputImageComponent'
+import InputImageComponent from '../components/InputImageComponent'
 import CanvasComponent from '../components/CanvasComponent'
+
+import * as InputImageActions from '../actions/InputImageActions'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {AppBar} from 'material-ui';
@@ -10,19 +14,34 @@ import {AppBar} from 'material-ui';
 
 
 
-export default class extends Component {
+class App extends Component {
     render() {
-        const { file } = this.props.image;
-        const titleBar = 'Лазерная гравировка';
+        const { setImage } = this.props.InputImageActions
+        const { file, width, height } = this.props.inputImage.image
+        const titleBar = 'Лазерная гравировка'
 
         return (
         <div>
             <MuiThemeProvider>
                 <AppBar showMenuIconButton={false} title={titleBar} />
             </MuiThemeProvider>
-            <CanvasComponent file={file} />
-            <InputImageComponent />
+            <CanvasComponent file={file} width={width} height={height} />
+            <InputImageComponent setImage={setImage} />
         </div>
         )
   }
 }
+
+function mapStateToProps(state) {
+    return {
+        inputImage: state.InputImageReducer
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        InputImageActions: bindActionCreators(InputImageActions, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
