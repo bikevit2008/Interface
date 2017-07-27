3a77d0be2b114c5f9cd5e632b7e45ee29c1462e2
Vitaly <bikevit2008@rambler.ru>
2017-07-27 16:09:00 +0300

    For size in props

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,34 +3,33 @@ import {Layer, Stage, Image} from 'react-konva';
 
 
 export default class Canvas extends Component {
- state = {
-  image: null,
-  width: 0,
-  height: 0
-}
-  componentDidUpdate() {
-  const image = new window.Image()
-  image.src = this.props.image.file
-  image.onload = () => {
-    this.setState({
-      image: image,
-      width: image.width,
-      height: image.height
-    });
-  }
-}
+    state = {
+        image: null
+    }
+    componentDidUpdate() {
+        const image = new window.Image()
+        image.src = this.props.image.file
+        image.onload = () => {
+            this.setState({
+                image: image
+            });
+        }
+    }
  
-  render() {
-    return <div className='ib page'>
-              <Stage width={this.state.width} height={this.state.height}>
-                  <Layer>
-                    <Image image={this.state.image} />
-                  </Layer>
-              </Stage>
-    </div>
-  }
+    render() {
+        const { width, height } = this.props.image
+        return (
+            <div>
+                <Stage width={width} height={height}>
+                    <Layer>
+                        <Image image={this.state.image} />
+                    </Layer>
+                </Stage>
+            </div>
+      )
+    }
 }
 
 Canvas.propTypes = {
-  image: PropTypes.object.isRequired
+    image: PropTypes.object.isRequired
 }
