52ff4c742c92eb46934749b060213bbf4c714dbc
Vitaly <bikevit2008@rambler.ru>
2017-07-27 16:09:28 +0300

    For size in props

--- src/components/InputImage.js
+++ src/components/InputImage.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 
 
 export default class InputImage extends Component {
-  onChangeFileImage(e) {
-    var files = e.target.files[0]
-    console.log(files)
-    var urlObj = URL.createObjectURL(files)
-    console.log(urlObj)
-    this.props.setImage(urlObj)
-  }
+    onChangeFileImage(e) {
+        var files = e.target.files[0]
+        var urlObj = URL.createObjectURL(files)
+        const img = new window.Image()
+        img.src = urlObj
+        img.onload = () => {
+            var image = { file: urlObj, width: img.width, height: img.height}
+            this.props.setImage(image)
+        }
+    }
  
     render() {
         return (
